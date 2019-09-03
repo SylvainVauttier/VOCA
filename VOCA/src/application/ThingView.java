98089633a24761e9fe7488de445213ddd6680431
SylvainVauttier <sylvain.vauttier@mines-ales.fr>
2019-09-03 16:03:18 +0200

    Mise au point du drag&drop

--- VOCA/src/application/ThingView.java
+++ VOCA/src/application/ThingView.java
@@ -1,17 +1,123 @@
 package application;
 
+
+import javafx.event.EventHandler;
+import javafx.geometry.Bounds;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.scene.transform.Translate;
+import model.IoTClassNames;
 import model.Thing;
 
 public class ThingView extends Rectangle {
 	
+	String name;
 	Thing thing = null;
+	Text label = null;
+	double labelOffset;
 	//ImagePattern icone;
 	
 	public ThingView(double x, double y) {
 		super(x,y,30,30);
+		setEffect(new DropShadow());
 		
+		label = new Text(x,y+40,IoTClassNames.names[VocaView.selectedIoTtool]);
+		Bounds lb = label.getBoundsInLocal();
+		labelOffset=15-lb.getWidth()/2;
+		label.setX(x+labelOffset);
+		
+		this.setOnMousePressed(mouseEvent->{
+			VocaView.pressedThing=this;
+		});
+		
+		this.setOnMouseReleased(new EventHandler<MouseEvent>() {
+		
+		public void handle(MouseEvent event) {
+			//Rectangle r = (Rectangle) event.getSource();
+			//r.setFill(Color.ALICEBLUE);
+			if (VocaView.draggedThing!=null) 
+			{
+				VocaView.centerPane.getChildren().remove(VocaView.dragRectangle);
+				VocaView.dragRectangle=null;
+				
+				double x = event.getX();
+				double y = event.getY();
+				
+				x -=15;
+				y -=15;
+				
+				if (x<0) x=0;
+				if (y<0) y=0;
+				
+				VocaView.draggedThing.move(x, y);
+				VocaView.draggedThing=null;
+				
+			}
+			VocaView.pressedThing=null;
+			event.consume();
+		}
+		
+		});
+		
+//		this.setOnDragDetected(mouseEvent->{
+//			System.out.println("Dragged");
+//			//Dragboard db = this.startDragAndDrop(TransferMode.NONE);
+//			//ClipboardContent content = new ClipboardContent();
+//			VocaView.draggedThing=this;
+//			//mouseEvent.consume();
+//		});
+		
+//		this.setOnDragDone(dragEvent->{
+//			System.out.println("Dragged done");
+//			double dx = dragEvent.getX();
+//			double dy = dragEvent.getY();
+//			
+//			dx -=15;
+//			dy -=15;
+//			
+//			if (dx<0) dx=0;
+//			if (dy<0) dy=0;
+//			
+//			Translate translate = new Translate();
+//			translate.setX(dx);
+//			translate.setY(dy);
+//			
+//			this.getTransforms().add(translate);
+//			
+//			dragEvent.consume();
+//		});
+		
+		this.setOnContextMenuRequested(contextMenuEvent->{
+			System.out.append("menu");
+			VocaView.thingContextMenu.show(this,contextMenuEvent.getScreenX(),contextMenuEvent.getScreenY());
+		});
+	}
+	
+
+	public void paint(Pane p)
+	{
+		p.getChildren().addAll(this,this.label);
+	}
+	
+	public void move(double x,double y)
+	{
+		this.setX(x);
+		this.setY(y);
+		this.label.setX(x+labelOffset);
+		this.label.setY(y+40);
+	}
+	
+	public void erase(Pane p)
+	{
+	  p.getChildren().removeAll(this,this.label);
 	}
 
 }
--- VOCA/src/application/VocaView.java
+++ VOCA/src/application/VocaView.java
@@ -1,18 +1,22 @@
 package application;
 
+
 import dao.DaoObjet;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
@@ -26,6 +30,8 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.scene.transform.Translate;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import model.IoTClassNames;
@@ -33,13 +39,25 @@ import model.Thing;
 
 public class VocaView {
 	
+	static ThingView draggedThing = null;
+	
+	static ContextMenu thingContextMenu = null;
+	
+	static Pane centerPane = null;
+	
+	static Rectangle dragRectangle = null;
+	
+	static int selectedIoTtool = -1;
+
+	public static ThingView pressedThing;
+	
 	BorderPane root;
 	VocaController controler;
 	//protected boolean paneDragged;
 	
 	//TabPane tp = new TabPane();
 	
-	int selectedIoTtool = -1;
+	
 	
 	public VocaView(Stage primaryStage, VocaController ctr) {
 		
@@ -55,6 +73,10 @@ public class VocaView {
 			buildCenterPane();
 				
 			buildIotPane();
+			
+			buildControlPane();
+			
+			buildContextMenu();
 			
 //			
 			
@@ -73,27 +95,7 @@ public class VocaView {
 //			
 //			root.setTop(mb);
 			
-			//VBox ctrl = new VBox();
-			GridPane ctrl = new GridPane();
-			//TilePane ctrl = new TilePane(Orientation.VERTICAL);
-			Label lb1 = new Label("Huteur :");
-			TextField huteur = new TextField("Huteur");
-			Label lb2 = new Label("Scénario :");
-			TextField scen = new TextField("Scénario");
-			
-			Button charger = new Button("Charger");
-			charger.setMaxWidth(Double.MAX_VALUE);
-			//charger.getStyleClass().add("round-red");
-			Button sauver = new Button("Sauver");
-			sauver.setMaxWidth(Double.MAX_VALUE);
-			//sauver.getStyleClass().add("round-red");
-			Button nouveau = new Button("Nouveau");
-			nouveau.setMaxWidth(Double.MAX_VALUE);
-			//nouveau.getStyleClass().add("round-red");
-			//ctrl.getChildren().addAll(lb1,huteur,lb2,scen,nouveau,charger,sauver);
-			ctrl.addColumn(0,lb1,huteur,lb2,scen,nouveau,charger,sauver);
-			
-			root.setRight(ctrl);
+//			
 			
 			primaryStage.show();
 			
@@ -101,6 +103,47 @@ public class VocaView {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
+	}
+
+	private void buildContextMenu() {
+		// TODO Auto-generated method stub
+		ContextMenu cm = new ContextMenu();
+		MenuItem it1 = new MenuItem("supprimer");
+		MenuItem it2 = new MenuItem("editer");
+		cm.getItems().addAll(it1,it2);
+		
+		thingContextMenu = cm;
+		
+		it1.setOnAction(actionEvent->{
+			//System.out.println(it1.getParentPopup().getOwnerNode().getClass().getName());
+			//centerPane.getChildren().remove(it1.getParentPopup().getOwnerNode());
+			((ThingView)it1.getParentPopup().getOwnerNode()).erase(centerPane);
+		});
+	}
+
+	private void buildControlPane() {
+		// TODO Auto-generated method stub
+		GridPane ctrl = new GridPane();
+		//TilePane ctrl = new TilePane(Orientation.VERTICAL);
+		Label lb1 = new Label("Huteur :");
+		TextField huteur = new TextField("Huteur");
+		Label lb2 = new Label("Scénario :");
+		TextField scen = new TextField("Scénario");
+		
+		Button charger = new Button("Charger");
+		charger.setMaxWidth(Double.MAX_VALUE);
+		//charger.getStyleClass().add("round-red");
+		Button sauver = new Button("Sauver");
+		sauver.setMaxWidth(Double.MAX_VALUE);
+		//sauver.getStyleClass().add("round-red");
+		Button nouveau = new Button("Nouveau");
+		nouveau.setMaxWidth(Double.MAX_VALUE);
+		//nouveau.getStyleClass().add("round-red");
+		//ctrl.getChildren().addAll(lb1,huteur,lb2,scen,nouveau,charger,sauver);
+		ctrl.addColumn(0,lb1,huteur,lb2,scen,nouveau,charger,sauver);
+		
+		root.setRight(ctrl);
 		
 	}
 
@@ -114,6 +157,7 @@ public class VocaView {
 				
 		Pane pane = new Pane();
 		pane.setBackground(bg);
+		centerPane=pane;
 		//pane.setMinSize(600, 400);
 		
 		pane.setMinSize(screen.getVisualBounds().getWidth(), screen.getVisualBounds().getHeight());
@@ -139,13 +183,13 @@ public class VocaView {
 		
 		//pane.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
 		
-		pane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+		pane.setOnMouseReleased(new EventHandler<MouseEvent>() {
 			
 			public void handle(MouseEvent event) {
 				
-				if (selectedIoTtool==-1) return;
+				//if (event.isConsumed()) return;
 				
-				//if (event.isDragDetect()) return;
+				if (selectedIoTtool==-1) return;
 				
 				
 				double x = event.getX();
@@ -157,29 +201,65 @@ public class VocaView {
 				if (x<0) x=0;
 				if (y<0) y=0;
 				
+				
+				if (draggedThing==null)
+				{
+				//Text text = new Text(x,y+40,"name");
 				ThingView view = ThingViewFactory.buildView(selectedIoTtool, x, y);
-				
-//				Image ic = new Image(getClass().getResourceAsStream("../img/Fauteuil.png"),30,30,true,true);
-//				
-//				Rectangle r = new Rectangle(x,y,30, 30);
-//				r.setFill(new ImagePattern(ic));
-//				
-//				r.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-//					
-//					public void handle(MouseEvent event) {
-//						Rectangle r = (Rectangle) event.getSource();
-//						r.setFill(Color.ALICEBLUE);
-//						event.consume();
-//					}
-//					
-//				});
-				
-				
-				pane.getChildren().add(view);
+				//view.name=IoTClassNames.names[selectedIoTtool];
+				//pane.getChildren().addAll(view,text);
+				view.paint(pane);
+				}
+				else
+				{
+//					Translate translate = new Translate();
+//					translate.setX(x);
+//					translate.setY(y);
+					
+					//draggedThing.getTransforms().add(translate);
+					pane.getChildren().remove(dragRectangle);
+					dragRectangle=null;	
+					
+					//draggedThing.setX(x);
+					//draggedThing.setY(y);
+					draggedThing.move(x, y);
+					draggedThing=null;
+				}
 				
 				DaoObjet.getDao().persist(new Thing());
 			}
 			
+		});
+		
+		pane.setOnMouseDragged(mouseEvent->{
+			
+			if (pressedThing!=null)
+			{
+				draggedThing=pressedThing;
+				pressedThing=null;
+			}
+			
+			double x = mouseEvent.getX();
+			double y = mouseEvent.getY();
+			
+			x -=15;
+			y -=15;
+			
+			if (x<0) x=0;
+			if (y<0) y=0;
+			
+			if (dragRectangle==null)
+			{
+				dragRectangle=new Rectangle(x,y,30,30);
+				dragRectangle.setFill(Color.TRANSPARENT);
+				dragRectangle.setStroke(Color.BLACK);
+				centerPane.getChildren().add(dragRectangle);
+			}
+			else
+			{
+				dragRectangle.setX(x);
+				dragRectangle.setY(y);
+			}
 		});
 	}
 
